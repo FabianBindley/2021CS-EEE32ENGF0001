45f85fdddf0da22de6cbe55d35a3decc4baac887
SilentHacks <therealsilenthacks@gmail.com>
2021-12-01 10:39:39 +0000

    Restructured code, added boilerplate function templates to allow communication with cloud easily

--- ArduinoSendEsp32/I2C_SlaveArduino.ino
+++ ArduinoSendEsp32/I2C_SlaveArduino.ino
@@ -1,71 +1,131 @@
+#include <Wire.h>
+
+
 // I2C slave Arduino
 #define SLAVE_ADDR 9
-#include <Wire.h>
-float degreeC=35.0;
+#define DELAY_TIME 100
+
+// Define sensors
+#define TEMP 1
+#define MOTOR 2
+#define PH 3
+
+
+float degreeC = 35.0;
 float RPM = 200.0;
 float PH = 7.0;
-float sensors[3] = {degreeC,RPM,PH};
+float sensors[3] = {degreeC, RPM, PH};
 int sensorNo = 0;
 
 
+float getTemp();
+float getRPM();
+float getPH();
+
+void setTemp();
+void setRPM();
+void setPH();
+
+
+float readSensor() {
+  float result = 0.0;
+  switch (sensorNo) {
+    case TEMP:
+      result = getTemp();
+      break;
+    case MOTOR:
+      result = getRPM();
+      break;
+    case PH:
+      result = getPH();
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
+
+
+void writeSensor() {
+  switch (sensorNo) {
+    case TEMP:
+      setTemp();
+      break;
+    case MOTOR:
+      setRPM();
+      break;
+    case PH:
+      setPH();
+      break;
+    default:
+      break;
+  }
+}
+
+
 void setup() {
- Wire.begin(SLAVE_ADDR); // join i2c bus with address #8
+  Wire.begin(SLAVE_ADDR); // Join i2c bus with slave address
  
   // Function to run when data requested from master
- Wire.onRequest(requestEvent); 
+  Wire.onRequest(requestEvent); 
   // Function to run when data received from master
- Wire.onReceive(receiveEvent);
+  Wire.onReceive(receiveEvent);
   
- Serial.begin(9600);
- Serial.println("I2C Slave Arduino");
+  Serial.begin(9600);
+  Serial.println("I2C Slave Arduino");
 }
 
 void loop() {
- delay(100);
+  delay(DELAY_TIME);
 }
 
 void requestEvent() {
- Wire.write((byte)sensors[sensorNo]); 
- Serial.println("Request event");
+  Serial.println("Request event");
+
+  float result = readSensor();
+  String result_str = String(result, 3); // Convert float to string with 3 DP precision
+
+  Wire.write(result_str); 
 }
 
 void receiveEvent() {
-    char received_string[10];
-    int counter = 0;
-    char *token;
-    float fvalue;
+  char received_string[10];
+  int counter = 0;
+  char *token;
+  float fvalue;
     
-    while (Wire.available()) {
-      received_string[counter++] = Wire.read();
-    }
+  while (Wire.available()) {
+    received_string[counter++] = Wire.read();
+  }
   
-    // 1st token is operation name in string
-   char* operation = strtok(received_string, " ");   
-   // 2nd token is sensor number in int
-   token = strtok(NULL, " ");
-   if (token != NULL){
-    int sensorNo = (token-'0') - 1;
-   }
-   // 3rd token is value in float
-   char* strvalue = strtok(NULL, " ");
-   if (strvalue != NULL){
+  // 1st token is operation name in string
+  char *operation = strtok(received_string, " ");
+
+  // 2nd token is sensor number in int
+  token = strtok(NULL, " ");
+  if (token != NULL){
+  int sensorNo = (token-'0') - 1;
+  }
+  
+  // 3rd token is value in float
+  char *strvalue = strtok(NULL, " ");
+  if (strvalue != NULL) {
     // str -> float
     fvalue = atof(strvalue);
-   }
-   
-  
+  }
   
   if (strcmp(operation, "SET") == 0){
-    sensors[sensorNo]=fvalue;
-    
+    writeSensor();
   }
   
   else if (strcmp(operation, "STOP") == 0){
-    // operation is STOP, raises SysExit
+    // Raise infinite loop to stop running of code
+    while (1);
   }
   
   Serial.println("Receive event");
-  }
+}
 
   
   
