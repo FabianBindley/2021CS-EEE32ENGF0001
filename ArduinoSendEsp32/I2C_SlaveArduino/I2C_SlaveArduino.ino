0a59692a418c8c0537b9abeceddd0fdba1dbcb9d
SilentHacks <therealsilenthacks@gmail.com>
2021-12-03 15:04:39 +0000

    Integrated temperature control

--- ArduinoSendEsp32/I2C_SlaveArduino/I2C_SlaveArduino.ino
+++ ArduinoSendEsp32/I2C_SlaveArduino/I2C_SlaveArduino.ino
@@ -13,20 +13,104 @@
 
 #define SERIAL_DEBUG_BAUD 115200
 
+
+// Define constants
+const float A = 0.0012323996168659172;
+const float B = 0.00022347497515516762;
+const float C = 8.354794023823214e-08;
+const float kelvinOffset = 273.15;
+const float voltageInput = 5;
+const int range = 1024;
+const int resistorValue = 10000;
+
+// Define variables
 int sensorNo = 0;
 
+int vInput = 0;
+int heaterOutputPin = 3;
+int thermistorInputPin = A0;
+int heaterLEDpin = 4;
 
-double getTemp();
-double getRPM();
-double getPH();
+float kelvinTemp;
+float celciusTemp;
+float thermistorResistance;
+float voltage;
 
-void setTemp();
-void setRPM();
-void setPH();
+float setpointTemperature = 30.0;
+float temperatureLagOffset = 1.0;
+
+float setpointRPM = 2400;
+float setpointPH = 7.0;
 
 
-double readSensor() {
-  double result = 0.0;
+void setTemp(float value) {
+  setpointTemperature = value;
+  Serial.print("Set temp to: ");
+  Serial.println(setpointTemperature);
+}
+
+
+float getTemp() {
+  vInput = analogRead(thermistorInputPin);
+  // delay(100); what's this for
+  voltage = vInput / (range / voltageInput);
+
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+  
+  //We have voltage now we need resistance
+  thermistorResistance = voltage * resistorValue / (voltageInput - voltage);
+  
+  kelvinTemp = 1 / (A + B * log(thermistorResistance) + C * (log(thermistorResistance) * log(thermistorResistance) * log(thermistorResistance)));
+  celciusTemp = kelvinTemp - kelvinOffset;
+
+  return celciusTemp;
+}
+
+
+void setRPM(float value) {
+  setpointRPM = value;
+  Serial.print("Set RPM to: ");
+  Serial.println(setpointRPM);
+}
+
+
+float getRPM();
+
+
+void setPH(float value) {
+  setpointPH = value;
+  Serial.print("Set pH to: ");
+  Serial.println(setpointPH);
+}
+
+
+float getPH();
+
+
+void tempControl() {
+  celciusTemp = getTemp();
+
+  Serial.print("Celcius Temp: ");
+  Serial.println(celciusTemp);
+  
+  //If the temperature is less than the required temp - the lag offset, turn on the heating circuit
+  if (celciusTemp < (setpointTemperature - temperatureLagOffset)) {
+    //Turn on the heater at half strength using 50% PWM
+    analogWrite(heaterOutputPin, 127); 
+    digitalWrite(heaterLEDpin, HIGH);
+    Serial.println("Heater On");
+  } else {
+    //Turn off the heater
+    Serial.println("Heater off");
+    digitalWrite(heaterLEDpin, LOW);
+    digitalWrite(heaterOutputPin, LOW);
+  }
+}
+
+
+float readSensor() {
+  float result = 0.0;
   switch (sensorNo) {
     case TEMP:
       result = getTemp();
@@ -45,16 +129,16 @@ double readSensor() {
 }
 
 
-void writeSensor() {
+void writeSensor(float value) {
   switch (sensorNo) {
     case TEMP:
-      setTemp();
+      setTemp(value);
       break;
     case MOTOR:
-      setRPM();
+      setRPM(value);
       break;
     case PH:
-      setPH();
+      setPH(value);
       break;
     default:
       break;
@@ -69,19 +153,28 @@ void setup() {
   Wire.onRequest(requestEvent); 
   // Function to run when data received from master
   Wire.onReceive(receiveEvent);
+
+  // Heater/thermistor pins
+  pinMode(heaterOutputPin,OUTPUT);
+  pinMode(heaterLEDpin,OUTPUT);
+  pinMode(thermistorInputPin,INPUT);
   
+  // Begin debug Serial Monitor
   Serial.begin(SERIAL_DEBUG_BAUD);
   Serial.println("I2C Slave Arduino");
 }
 
 void loop() {
   delay(DELAY_TIME);
+
+  // Handle control of sensors
+  tempControl();
 }
 
 void requestEvent() {
   Serial.println("Request event");
 
-  double result = readSensor();
+  float result = readSensor();
 
   char result_str[10];
   dtostrf(result, 4, 4, result_str);  //4 is mininum width, 4 is precision; float value is copied onto buff
@@ -118,7 +211,7 @@ void receiveEvent(int num_bytes) {
   }
   
   if (strcmp(operation, "SET") == 0){
-    writeSensor();
+    writeSensor(fvalue);
   }
   
   else if (strcmp(operation, "STOP") == 0){
@@ -126,8 +219,3 @@ void receiveEvent(int num_bytes) {
     while (1);
   }
 }
-
-  
-  
-  
- 
